9e256cd2b7574ae417b5b9a79287122d21345950
andrei1994rus <rusakov.andrei@hotmail.com>
2021-12-15 19:33:38 +0300

    refactor footer (text)

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 const footer=()=>
 (
-	<h1 id="footer_text">
-		This app uses <a className="link_api" href="https://github.com/hitbtc-com/hitbtc-api" 
-		target="_blank" rel="noopener noreferrer">api</a>.  	
-	</h1>	
+	<div>
+		<span id="footer_text">
+			This app uses <a className="link_api" href="https://github.com/hitbtc-com/hitbtc-api" 
+			target="_blank" rel="noopener noreferrer">api</a>.  	
+		</span>
+		<span id="second_footer_text">
+			Page List crypto has horizontal scroll 
+			for mobile devices.
+		</span>
+	</div>	
 )
 
 export default footer;
